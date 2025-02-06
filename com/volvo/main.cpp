6fd289b47c226990611eb662750c41d889164c2c
jijosg <jijosunny90@gmail.com>
2025-02-06 22:01:41 +0100

    Update project version to 0.2 and change C++ standard to C++17; enhance main program to read user input

--- com/volvo/main.cpp
+++ com/volvo/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::cout << __cplusplus << std::endl;
+    // Read a number from the user
+    int number;
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+    printf("You entered: %02d\n", number);
 }
